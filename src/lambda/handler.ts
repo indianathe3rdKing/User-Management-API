359f266406a4178d3336c431364996021e92d964
indianathe3rd <indianathe3rd@gmail.com>
2025-08-02 17:34:55 +0200

    default function

--- src/lambda/handler.ts
+++ src/lambda/handler.ts
@@ -25,34 +25,39 @@ export const handler = async (
       }
     }
 
-    if (path.startsWith("/users/")){
-        const userId = path.split("/")[1]
-        if(!userId){
-            return{
-                statusCode: 400,
-                body: JSON.stringify({
-                    message: "User ID is required"
-                })
-            }
-        }
+    if (path.startsWith("/users/")) {
+      const userId = path.split("/")[1];
+      if (!userId) {
+        return {
+          statusCode: 400,
+          body: JSON.stringify({
+            message: "User ID is required",
+          }),
+        };
+      }
 
-        switch(method){
-            case "GET":
-                return getUser(userId)
-            case "PUT":
-                return updateUser(event,userId)
-            case "DELETE"
-                return deleteUser(userId)        
-            default:
-                return{
-                    statusCode: 400,
-                    body: JSON.stringify({
-                        message: `Unsupported HTTP method for /users/${userId} path`,
-                    })
-                }
-
-        }
+      switch (method) {
+        case "GET":
+          return getUser(userId);
+        case "PUT":
+          return updateUser(event, userId);
+        case "DELETE":
+          return deleteUser(userId);
+        default:
+          return {
+            statusCode: 400,
+            body: JSON.stringify({
+              message: `Unsupported HTTP method for /users/${userId} path`,
+            }),
+          };
+      }
     }
+    return {
+      statusCode: 404,
+      body: JSON.stringify({
+        message: "Path not found",
+      }),
+    };
   } catch (error) {
     console.error("Error processing request:", error);
     return {
@@ -63,7 +68,6 @@ export const handler = async (
     };
   }
 };
-
 
 async function getAllusers(
   event: APIGatewayProxyEventV2
@@ -76,41 +80,43 @@ async function getAllusers(
   };
 }
 
-async function createUser(event:APIGatewayProxyEventV2):Promise<APIGatewayProxyResultV2> {
-    return{
-        statusCode:201,
-        body:JSON.stringify({
-            message: "User created successfully"
-        })
-    }
-    
+async function createUser(
+  event: APIGatewayProxyEventV2
+): Promise<APIGatewayProxyResultV2> {
+  return {
+    statusCode: 201,
+    body: JSON.stringify({
+      message: "User created successfully",
+    }),
+  };
 }
 
-async function updateUser(event:APIGatewayProxyEventV2,userId:string):Promise<APIGatewayProxyResultV2> {
-    return{
-        statusCode:200,
-        body: JSON.stringify({
-            message: `User with ID ${userId} updated successfully`
-        })
-    }
+async function updateUser(
+  event: APIGatewayProxyEventV2,
+  userId: string
+): Promise<APIGatewayProxyResultV2> {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: `User with ID ${userId} updated successfully`,
+    }),
+  };
 }
 
-async function getUser(userId:string):Promise<APIGatewayProxyResultV2> {
-    return{
-        statusCode: 200,
-        body: JSON.stringify({
-            message: `Get user with ID ${userId}`,
-        })
-    }
-    
+async function getUser(userId: string): Promise<APIGatewayProxyResultV2> {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: `Get user with ID ${userId}`,
+    }),
+  };
 }
 
-async function deleteUser(userId:string):Promise<APIGatewayProxyResultV2> {
-    return{
-        statusCode:200,
-        body: JSON.stringify({
-            message: `User with ID ${userId} has been deleted successfully`,
-        })
-    }
-    
+async function deleteUser(userId: string): Promise<APIGatewayProxyResultV2> {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: `User with ID ${userId} has been deleted successfully`,
+    }),
+  };
 }

7cbec8b8dfca570685193a1ea58d8e3f6bbde94b
indianathe3rd <indianathe3rd@gmail.com>
2025-08-02 21:41:09 +0200

    Define Stack

--- lib/users management api-stack.ts
+++ lib/users management api-stack.ts
@@ -1,16 +1,79 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import { he } from "@faker-js/faker";
+import * as cdk from "aws-cdk-lib";
+import { Runtime } from "aws-cdk-lib/aws-lambda";
+import { Construct } from "constructs";
+import path from "path";
+import { handler } from "../src/lambda/handler";
+import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
+import * as apigateway from "aws-cdk-lib/aws-apigatewayv2";
+import * as apigateway_integrations from "aws-cdk-lib/aws-apigatewayv2-integrations";
+import { Method } from "aws-cdk-lib/aws-apigateway";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class UsersManagementApiStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    //Create a single lambda function to handle all user management operations
+    const userHandler = new NodejsFunction(this, "UserHandler", {
+      runtime: Runtime.NODEJS_22_X,
+      entry: path.join(__dirname, "../src/lambda/handler.ts"),
+      handler: "handler",
+      functionName: `${this.stackName}-user-handler`,
+    });
+    const httpApi = new apigateway.HttpApi(this, "UserApi", {
+      apiName: "User API",
+      description: "User management API",
+      corsPreflight: {
+        allowOrigins: ["*"],
+        allowMethods: [apigateway.CorsHttpMethod.ANY],
+        allowHeaders: ["*"],
+      },
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'UsersManagementApiQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const routes = [
+      {
+        path: "/users",
+        method: apigateway.HttpMethod.GET,
+        name: "GetAllUsers",
+      },
+      {
+        path: "/users",
+        method: apigateway.HttpMethod.POST,
+        name: "CreateUser",
+      },
+      {
+        path: "/users/{id}",
+        method: apigateway.HttpMethod.PUT,
+        name: "updateUser",
+      },
+      {
+        path: "/users/{id}",
+        method: apigateway.HttpMethod.DELETE,
+        name: "deleteUser",
+      },
+      {
+        path: "/users/{id}",
+        method: apigateway.HttpMethod.GET,
+        name: "getUser",
+      },
+    ];
+
+    //Add all routes to the API
+    routes.forEach(({ path, method, name }) => {
+      httpApi.addRoutes({
+        path,
+        methods: [method],
+        integration: new apigateway_integrations.HttpLambdaIntegration(
+          `${name}Integration`,
+          userHandler
+        ),
+      });
+    });
+    //Output the API URL
+    new cdk.CfnOutput(this, "HttpApiUrl", {
+      value: httpApi.url ?? "",
+      description: "HTTP API URL",
+    });
   }
 }
